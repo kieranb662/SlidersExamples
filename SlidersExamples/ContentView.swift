24b222f491a7c82bcfd63a09512b0117cab21414
Kieran Brown <kb6627500@gmail.com>
2020-04-11 15:21:16 -0400

    Initial Commit

---
+++ SlidersExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlidersExamples
+//
+//  Created by Kieran Brown on 4/11/20.
+//  Copyright © 2020 BrownandSons. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
